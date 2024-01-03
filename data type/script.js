abd464d6d2bbacab45fa7fdafdf110dd713e609e
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-03 22:18:03 +0100

    Added data type (string, number, boolean) , toFixed, topresion!

---
+++ data type/script.js
@@ -0,0 +1,56 @@
+//1. write function
+
+document.write("Name: Abdulla Al Harun Mehedi <br>");
+document.write("Phone: +47 000000 <br>");
+document.write("Adress: Teppavien 9");
+
+
+// ###----DATA TYPE----###
+//string 
+let greeting ="Hello World";
+console.log(typeof greeting);
+
+//number data type 
+let numberData = 888;
+console.log(typeof numberData);
+
+//bollean data type
+let isHappy = true;
+console.log(typeof isHappy);
+
+//Variables 
+// Variable name should follow 
+// 1) letter
+// 2) number 
+// _ 
+// $ 
+const myName = "abdulla"
+const myAge = 31;
+console.log(myName);
+console.log(myAge);
+
+// Number Method 
+// number convert by strings
+var num = 44;
+num = toString(num); // number convert by strings
+console.log(typeof num);
+
+//string converted by intiger 
+var num2 = "22";
+num2 = parseInt(num);
+console.log(typeof num2);
+
+
+//string converted by intiger (decimel / .)
+var num3 = "11.11";
+num3 = parseFloat(num3);
+console.log(typeof num3);
+
+var number = 8.9777
+
+// toFixed means after . how many number can take! 
+console.log(number.toFixed(2));
+
+
+
+
