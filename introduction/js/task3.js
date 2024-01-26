a790e0da42acf45a87ff27eddb083fcf5af7178c
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-26 01:32:16 +0100

    task 3 done!

---
+++ introduction/js/task3.js
@@ -0,0 +1,22 @@
+// Task-3
+// You task is to divide the given number by 5 and show the remainder as the output.
+
+// Input:
+// The first line of the input contains the number.
+
+// Output:
+// Print the remainder.
+
+// Sample Input:
+// 119
+
+// Sample Output:
+// 5
+
+var givenNum = 119; 
+
+remainder = givenNum % 5;
+
+console.log(remainder);
+
+//answer is 4 
