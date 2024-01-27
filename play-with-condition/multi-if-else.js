4614a5eb6b61d0243f300360c48e523e9f793760
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-27 14:24:10 +0100

    adde multi level if else example!!

---
+++ play-with-condition/multi-if-else.js
@@ -0,0 +1,11 @@
+const age = 50; 
+const price = 500;
+
+if (age <= 12 ) {
+    console.log("you can eat free");
+} else if (age >= 60) {
+    console.log(price * 50 / 100);
+} else if (age == 50) {
+    console.log(price * 40 / 100);
+} else 
+console.log("You have to pay 100%")
