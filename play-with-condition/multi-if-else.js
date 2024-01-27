3d1560405f4b5bc5040b7fb5d6c15ab8be50f34c
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-27 15:27:34 +0100

    Nested If else!

--- play-with-condition/multi-if-else.js
+++ play-with-condition/multi-if-else.js
@@ -9,3 +9,18 @@ if (age <= 12 ) {
     console.log(price * 40 / 100);
 } else 
 console.log("You have to pay 100%")
+
+// Nasted If else !! 
+const money =  12 ;
+
+if (money > 500){
+    console.log("you are reach!!");
+}else {
+    if (money > 200){
+        console.log("tui gorib o na abar boror lok o na!!")
+    } else if (money > 100) {
+        console.log("tui almost gorib!");
+    } else {
+        console.log("tui hala jonmer fokira!");
+    }
+}
