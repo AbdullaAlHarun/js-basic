806481724f4fd2a38271cf86b07916ff4bf7c067
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-30 16:01:16 +0100

    added some example of strings!

---
+++ string-n-objects/slice.js
@@ -0,0 +1,21 @@
+const address= 'andorkilla';
+const part = address.slice(2,5);
+// console.log(part);
+
+const sentence = "I am good and hardworking person.";
+
+// console.log(sentence.split(' '));
+
+const friendsStr = 'Rahim,kahim,dahim,lahim,fahim,sahim';
+const friends = friendsStr.split(',');
+console.log(friends);
+console.log(friends);
+console.log(friends.join());
+console.log(friends.join('|'));
+console.log(friends.join('-'));
+
+const first ='abid';
+const last ='nabid';
+
+const fullName = first + ' ' + last;
+console.log(fullName);
---
+++ string-n-objects/string.js
@@ -0,0 +1,12 @@
+const country = "Bangladesh";
+const division = 'Noakhali';
+const district = `B-baria`;
+const thana = new String('Demra');
+
+// const numbers = [54, 66, 78 , 90];
+// console.log(numbers.length);
+// console.log(numbers[1]);
+
+const capital = 'Dhaka';
+console.log(capital.length);
+console.log(capital[2]);
