fa016dc96767ceb194743548097fb6026ebdd8e8
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-30 14:30:23 +0100

    fixed some issue!!

--- array/scripts.js
+++ array/scripts.js
@@ -15,4 +15,17 @@ friends.pop();
 console.log(friends); // remove last data 
 
 friends.shift();
+console.log(friends); // remove from beganning 
+
+friends.unshift("khailam"); // add from beganning 
 console.log(friends);
+
+console.log(friends.length);
+
+console.log(friends.includes("salam"));
+
+if (friends.includes("rahim")){
+    console.log("khela hobe!!!");
+}else {
+    console.log("Ghumabo!!!")
+}
