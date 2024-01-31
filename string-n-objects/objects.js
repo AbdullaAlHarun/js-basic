30a75a2a63441ad5a408fd53129d0dbf09f9a25f
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-01-31 12:13:07 +0100

    objects example!!

---
+++ string-n-objects/objects.js
@@ -0,0 +1,14 @@
+const age = 21; 
+const school = "zila school";
+const isPassed = true;
+
+const subjects = ['bangla', 'English', 'Physics', 'Math'];
+
+//objects 
+
+const bottle = {
+    brand: 'apple',
+    price: 45,
+    color: 'white',
+    isClear: false
+}
