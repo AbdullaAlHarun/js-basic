724cfec92e0c5769d0872201bff3a3c6cc025435
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-02 15:59:34 +0100

    add some function example !

---
+++ function/function.js
@@ -0,0 +1,24 @@
+// const number = 45;
+
+// function fanOffKor(){
+//     console.log('uthe darao')
+//     console.log('bose jao')
+//     console.log('daat brash koro')
+// }
+
+// fanOffKor();
+
+// function square(number){
+//     console.log(number);
+//     const borgo = numbur * number;
+//     console.log(borgo);
+// }
+
+// square(4)
+
+function addAll (a,b,c,d,e){
+    const total = a+ b+ c + d+ e;
+    console.log(total);
+}
+
+addAll(2,2,2,2,2)
