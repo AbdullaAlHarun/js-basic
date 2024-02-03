b6c2a3986e7100ce2cbf79b9d26823efa63f9c1a
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-03 18:15:47 +0100

    return stop concept!!

---
+++ function/obj-fun.js
@@ -0,0 +1,10 @@
+function hello(){
+
+    console.log("before return");
+    return 'hello';
+    console.log("after return");
+}
+
+console.log(hello());
+
+// retur stop the exucution of functiom imidietly ! 
