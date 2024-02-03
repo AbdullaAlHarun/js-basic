4cbee520c4d57199cccf1474beb3bfe77031a6ad
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-03 18:26:28 +0100

    inche to feet!!

--- function/obj-fun.js
+++ function/obj-fun.js
@@ -7,4 +7,23 @@ function hello(){
 
 console.log(hello());
 
-// retur stop the exucution of functiom imidietly ! 
+// return stop the exucution of functiom imidietly ! 
+
+function inchToFeet (inch){
+    const feet = inch / 12; 
+    return feet;
+}
+
+function inchToFeet2 (inch){
+    const feetFraction = inch / 12; 
+    const feetNumber = parseInt(feetFraction)
+    const inchReaming = inch %12;
+    const result = feetNumber + 'ft' + inchReaming + 'inch';
+    return result;
+} 
+
+const shuvoHeight = inchToFeet(75);
+
+const shuvoHeight2 = inchToFeet(75);
+
+console.log(shuvoHeight2)
