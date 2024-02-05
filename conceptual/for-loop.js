d08cc6cd7914e2b789aed5878d951ca9b49fd559
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-05 19:35:01 +0100

    Example 1 & 2 : for loop!!

---
+++ conceptual/for-loop.js
@@ -0,0 +1,18 @@
+// // Example 1 : 
+
+// for (let i = 1; i <=10; i++){
+//     console.log("Abdulla!")
+// } 
+
+// // Example 2 
+// for (let i = 1; i <= 100; i++){
+//     console.log("Sorry!!.........!!")
+// }
+
+// Example : 3 
+
+const nums = [23, 43, 2, 5,345,203,2];
+
+for(let i =0; i <= nums.length; i++){
+    console.log(nums[i]);
+}
