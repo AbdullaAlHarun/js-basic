75f5f0e6c21680fe5b7fd1e9f9268a8c251e3504
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-05 19:24:55 +0100

    Example 1: While loop!

---
+++ conceptual/while.js
@@ -0,0 +1,16 @@
+// let i = 1; 
+
+// while (i <= 5){
+//     console.log("Mehedi");
+//     i++;
+// }
+
+let i = 1;
+let sum = 0;
+
+while (i<=10){
+    sum += i;
+    i++;
+}
+
+console.log(sum);
