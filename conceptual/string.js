cccd4fd515cecb72611f407ea6d78de866c1a83b
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-05 23:05:54 +0100

    trim function added!

--- conceptual/string.js
+++ conceptual/string.js
@@ -9,3 +9,20 @@ if (x.toLowerCase == y.toLowerCase){
 {
     console.log("They are different!")
 }
+
+const a = "Hello World";
+const b = "hello";
+
+// console.log(x.includes(y));
+console.log(x.toLowerCase().includes(y.toLowerCase()));
+
+const aa = "hello";
+const bb = " hello "
+
+if (aa === bb.trim()){
+    console.log("Both are same");
+} else 
+{
+    console.log("They are different!")
+}
+
