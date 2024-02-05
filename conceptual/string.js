ce23e09bdd0a6e10e586a0102194acae2b85e1d5
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-05 22:58:51 +0100

    toLowerCase added!!

---
+++ conceptual/string.js
@@ -0,0 +1,11 @@
+
+// toLowerCase 
+const x = "hello";
+const y = "hEllo";
+
+if (x.toLowerCase == y.toLowerCase){
+    console.log("Both are same");
+} else 
+{
+    console.log("They are different!")
+}
