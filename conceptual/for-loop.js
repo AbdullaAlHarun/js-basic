9ed39437aaf81f294091860d2e3b001bf608ca7d
Abdulla Al Harun <abdhar01276@stud.noroff.no>
2024-02-05 20:23:32 +0100

    Example of for of loop!!

--- conceptual/for-loop.js
+++ conceptual/for-loop.js
@@ -13,6 +13,24 @@
 
 const nums = [23, 43, 2, 5,345,203,2];
 
-for(let i =0; i <= nums.length; i++){
-    console.log(nums[i]);
+let sumOfOdd = 0;
+let sumOfEven = 0;
+
+for(let i =0; i < nums.length; i++){
+    const x = nums[i];
+
+    if (x%2 === 1){
+        sumOfOdd += x;
+    }else {
+        sumOfEven += x;  
+    }
+}
+
+console.log(sumOfEven);
+console.log(sumOfOdd);
+
+//for 0f loop
+const num = [23, 43, 2, 5,345,203,2];
+for (let item of num) {
+    console.log(item);
 }
